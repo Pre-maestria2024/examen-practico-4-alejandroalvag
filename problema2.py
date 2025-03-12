741804d338d8c1ab0e98172339fff87bcf7f2af9
alejandroalvag <149961520+alejandroalvag@users.noreply.github.com>
2025-03-12 08:40:46 -0500

    Update problema2.py

--- problema2.py
+++ problema2.py
@@ -1,12 +1,45 @@
-from queue import PriorityQueue
-from queue import Queue
+from queue import PriorityQueue, Queue
 
 def main():
-
     n, k = map(int, input().split())
+    adj = [[] for _ in range(n)]
+    for _ in range(n-1):
+        u, v = map(int, input().split())
+        adj[u].append(v)
+        adj[v].append(u)
     
-    for i in range(n-1):
-        u,v = map(int,input().split())
+    depth = [0] * n
+    parent = [ -1 ] * n
+    q = Queue()
+    q.put(0)
+    while not q.empty():
+        u = q.get()
+        for v in adj[u]:
+            if v != parent[u]:
+                parent[v] = u
+                depth[v] = depth[u] + 1
+                q.put(v)
+    
+    pq = PriorityQueue()
+    for i in range(n):
+        pq.put((-depth[i], i))
+    
+    used = [False] * n
+    groups = 0
+    
+    while not pq.empty():
+        _, u = pq.get()
+        if not used[u]:
+            count = 0
+            current = u
+            while current != -1 and count < k and not used[current]:
+                used[current] = True
+                count += 1
+                current = parent[current]
+            if count == k:
+                groups += 1
+    
+    print(groups)
 
-if __name__  == '__main__':
+if __name__ == '__main__':
     main()
